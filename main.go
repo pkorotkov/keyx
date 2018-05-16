0c8a3c190b1eb1e573ff377b0e7b450264158174
pkorotkov <pkorotkov@gmail.com>
2018-05-16 14:32:50 +0300

    Add more hash algorithms

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha1"
+	"crypto/sha256"
 	"fmt"
 	"os"
 
@@ -12,24 +14,25 @@ import (
 const version = "0.0.1"
 
 const usage = "" +
-	`keyx computes funny hash of input data.
+	`keyx computes a hash of input data.
 
 Usage:
-  keyx [--no-prefix] -c <case> | --case=<case>
+  keyx [--no-prefix] [--upper-case] [--hash=<hash>]
   keyx -v | --version
   keyx -h | --help
 
 Options:
-  --no-prefix               Do not use prefix before reading data from stdin. 
-  -c <case>, --case=<case>  Set letter case used in hash string: 0 is lower, 1 is upper.
-  -v, --version             Show version.
-  -h, --help                Show this screen.
+  --no-prefix    Do not use prefix before reading data from stdin. 
+  --upper-case   Use upper case (instead of lower case) for hex string.
+  --hash=<hash>  Set the hash algorithm (blake2b, sha1, sha256) [default: blake2b].
+  -v, --version  Show version.
+  -h, --help     Show this screen.
 `
 
 func main() {
 	arguments, err := docopt.ParseDoc(usage)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "bad arguments")
+		fmt.Fprintln(os.Stderr, "error: bad arguments")
 		os.Exit(1)
 	}
 	if arguments["--version"].(bool) {
@@ -43,14 +46,24 @@ func main() {
 	}
 	reader := bufio.NewReader(os.Stdin)
 	data, _ := reader.ReadBytes('\n')
-	hash := blake2b.Sum256(data)
-	switch c := arguments["--case"]; {
-	case c == "0":
-		fmt.Printf("%x\n", hash)
-	case c == "1":
-		fmt.Printf("%X\n", hash)
+	var hash []byte
+	switch arguments["--hash"] {
+	case "blake2b":
+		h := blake2b.Sum256(data)
+		hash = h[:]
+	case "sha1":
+		h := sha1.Sum(data)
+		hash = h[:]
+	case "sha256":
+		h := sha256.Sum256(data)
+		hash = h[:]
 	default:
-		fmt.Fprintln(os.Stderr, "bad case indicator")
+		fmt.Fprintln(os.Stderr, "error: unknown hash algorithm")
 		os.Exit(2)
+	}
+	if !arguments["--upper-case"].(bool) {
+		fmt.Printf("%x\n", hash)
+	} else {
+		fmt.Printf("%X\n", hash)
 	}
 }
